8655dd00b56f059b25d580a16239556092ec58a6
leandroledo <lsledo@gmail.com>
2025-12-03 17:13:28 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home(){
-    return <h1>Just do It now</h1>
+function Home() {
+  return <h1>Just do It now</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 export default Home;
-
